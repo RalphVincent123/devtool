a77e6963106c9ef3984c539efba81a08213da520
Darryl <darryl.ofamen@gmail.com>
2024-10-18 09:10:52 +0800

    refactor getMappedUrl logic

--- src/logic/index.ts
+++ src/logic/index.ts
@@ -9,24 +9,14 @@ export const getMappedUrl = (
     const newUrl = newUrls[index]; // Get the corresponding new URL
     const regex = new RegExp(`href="${url}"`, "g");
 
-    updatedHtml = updatedHtml
-      .replace(regex, `href="${newUrl.replace("www.allaboutvision.com", "")}"`)
-      .replace(/<(h[1-3])>\s*<strong>(.*?)<\/strong>\s*<\/\1>/g, "<$1>$2</$1>");
+    updatedHtml = updatedHtml.replace(
+      regex,
+      `href="${newUrl.replace("www.allaboutvision.com", "")}"`
+    );
   });
 
-  return updatedHtml;
+  return updatedHtml.replace(
+    /<(h[1-3])>\s*<strong>(.*?)<\/strong>\s*<\/\1>/g,
+    "<$1>$2</$1>"
+  );
 };
-
-// let updatedHtmlString = htmlString;
-
-// // Replace each old URL with the corresponding new URL
-// oldUrls.forEach((oldUrl, index) => {
-//   const newUrl = newUrls[index]; // Get the corresponding new URL
-//   const regex = new RegExp(`href="${oldUrl}"`, "g");
-
-//   // Replace only if the old URL matches; otherwise, leave it unchanged
-//   updatedHtmlString = updatedHtmlString.replace(
-//     regex,
-//     `href="${newUrl.replace("www.allaboutvision.com", "")}"`
-//   );
-// });
