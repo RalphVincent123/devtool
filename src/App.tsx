13b45befadc0460a80cdc7713f3d06e09e6a5797
Ralph Vincent Pagcaliwagan <ralph.pagcaliwagan@ext.essilor.com>
2025-05-05 10:43:13 +0800

    add ES locale

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,3 @@
-// import { useEffect, useState } from "react";
-// import Editor from "./components/ui/Editor";
-// import { Label } from "./components/ui/label";
-// import { Textarea } from "./components/ui/Textarea";
-// // import { newUrls, oldUrls } from "./constants";
-// import urlsMapping from "./constants";
-// import { getMappedUrl } from "./logic";
-
-// const App = () => {
-//   const [inputHtml, setInputHtml] = useState("");
-//   const [outputHtml, setOutputHtml] = useState("");
-//   const [lang, setLang] = useState<"en" | "de" | "ru">("en");
-
-//   useEffect(() => {
-//     if (typeof window !== "undefined") {
-//       const detectedLang = window.location.pathname.split("/")[1]; // Get first part after domain
-//       if (["en", "de", "ru"].includes(detectedLang)) {
-//         setLang(detectedLang as "en" | "de" | "ru");
-//       } else {
-//         setLang("en"); // Default to English if no valid language found
-//       }
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     if (urlsMapping[lang]) {
-//       const mappedValue = getMappedUrl(
-//         urlsMapping[lang].oldUrls,
-//         urlsMapping[lang].newUrls,
-//         inputHtml
-//       );
-//       setOutputHtml(mappedValue);
-//     }
-//   }, [lang, inputHtml]);
-
-//   return (
-//     <main className="w-screen h-screen p-10 flex flex-col justify-center">
-//       <h2 className="flex justify-center">
-//         {lang === "en"
-//           ? "English "
-//           : lang === "de"
-//           ? "German "
-//           : lang === "ru"
-//           ? "Russia "
-//           : ""}
-//       </h2>
-//       <div className="flex justify-around w-full h-full">
-//         <div className="w-[32%] h-full">
-//           <Label className="block mb-2 w-full">Editor:</Label>
-//           <Editor className="h-full w-full" />
-//         </div>
-
-//         <div className="w-[32%] h-full">
-//           <Label htmlFor="input" className="block mb-2">
-//             Input:
-//           </Label>
-//           {/* <Textarea
-//             className="h-full w-full"
-//             onChange={(e) => setInputHtml(e.target.value)}
-//             id="input"
-//           /> */}
-//           <Textarea
-//             className="h-full w-full"
-//             value={inputHtml}
-//             onChange={(e) => setInputHtml(e.target.value)}
-//             id="input"
-//           />
-//         </div>
-
-//         <div className="w-[32%] h-full">
-//           <Label htmlFor="output" className="block mb-2">
-//             Output:
-//           </Label>
-//           {/* <Textarea
-//             className="h-[85%]"
-//             value={mappedValue}
-//             onChange={() => {}}
-//             id="output"
-//           /> */}
-//           <Editor data={outputHtml} className="h-full w-full" />
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default App;
-
 import { useEffect, useState } from "react";
 import Editor from "./components/ui/Editor";
 import { Label } from "./components/ui/label";
@@ -96,9 +8,9 @@ import { getMappedUrl } from "./logic";
 const App = () => {
   const [inputHtml, setInputHtml] = useState("");
   const [outputHtml, setOutputHtml] = useState("");
-  const [lang, setLang] = useState<"en" | "de" | "ru" | "contentReleaseEN">(
-    "en"
-  );
+  const [lang, setLang] = useState<
+    "en" | "de" | "ru" | "contentReleaseEN" | "es"
+  >("en");
 
   useEffect(() => {
     if (urlsMapping[lang]) {
@@ -145,7 +57,7 @@ const App = () => {
           />
           German
         </label>
-        <label>
+        <label className="mr-4">
           <input
             type="radio"
             name="language"
@@ -154,6 +66,16 @@ const App = () => {
             onChange={() => setLang("ru")}
           />
           Russian
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="language"
+            value="es"
+            checked={lang === "es"}
+            onChange={() => setLang("es")}
+          />
+          Spanish
         </label>
       </div>
       <div className="flex justify-around w-full h-full">
--- src/logic/index.ts
+++ src/logic/index.ts
@@ -1,27 +1,3 @@
-// export const getMappedUrl = (
-//   oldUrls: string[],
-//   newUrls: string[],
-//   html: string
-// ) => {
-//   let updatedHtml = html;
-
-//   oldUrls.forEach((url, index) => {
-//     const newUrl = newUrls[index]; // Get the corresponding new URL
-//     const regex = new RegExp(`href="${url}"`, "g");
-
-//     updatedHtml = updatedHtml.replace(
-//       regex,
-//       `href="${newUrl.replace("www.allaboutvision.com", "")}"`
-//     );
-//   });
-
-//   return updatedHtml.replace(
-//     /<(h[1-3])>\s*<strong>(.*?)<\/strong>\s*<\/\1>/g,
-//     "<$1>$2</$1>"
-//   );
-// };
-
-
 export const getMappedUrl = (
   oldUrls: string[],
   newUrls: string[],
